3a42132eaba3ed7422f92eb5b032a11b3a44e8e5
Yukiko Suzuki <yukinko.iwasaki@gmail.com>
2023-11-08 20:14:22 -0500

    announcements automate

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-
 window.onload = onLoad();
 
 function onLoad(){
     addCollapsibles();
     addAnnouncement();
 }
+    
+
+
 
 function addCollapsibles() {
     const collapsibles = document.getElementsByClassName("collapsibile-title");
@@ -28,14 +30,37 @@ function addCollapsibles() {
 }
 
 function addAnnouncement(){
-    const announce = document.getElementsByClassName("announcement");
-    const accounceDate = new Date('2023-11-08 GMT-0500').toDateString()
-    if (announce){
-        const today = new Date().toDateString();
-        if(today === accounceDate){
-            announce[0].className = 'announcement active'
-        }else{
-            announce[0].className = 'announcement'
+    
+    const ANNOUNCEMENTS = {
+        office_hour1:{
+            start_datetime: "2023-11-8",
+            end_datetime: "2023-12-13",
+            content: "Office Hour happening today at 2pm (EST)."
+        },
+        office_hour2:{
+            start_datetime: "2023-11-30",
+            end_datetime: "2023-12-13",
+            content: "Office Hour happening today at 2pm (EST)."
+        },
+        quizzes:{
+            start_datetime: "2023-11-8",
+            end_datetime: "2023-12-13",
+            content: "All quizzes are due 13 December, 11pm (EST) "
+        }
+    }
+    const announceElement = document.getElementsByClassName("announcement");
+
+    for (const key in ANNOUNCEMENTS){
+        const announce = ANNOUNCEMENTS[key]
+        const announceStart = new Date(announce.start_datetime)
+        const announceEnd = new Date(announce.end_datetime);
+        const now = new Date();
+        if (announceStart < now && announceEnd > now){
+            announceElement[0].className = 'announcement active'
+            const list = announceElement[0].getElementsByTagName("ul")?.[0]
+            const item = document.createElement('li')
+            item.textContent = announce.content
+            list.appendChild(item)
         }
     }
 }
